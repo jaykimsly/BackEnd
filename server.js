d1fc93f9c6a874f65ced8fbb2e8a7f34e0dff71e
joachim <joachim.mhlongo@thedigitalacademy.co.za>
2022-07-05 00:05:59 +0200

    before login

--- server.js
+++ server.js
@@ -1,9 +1,39 @@
 const express = require('express')
 const path =  require('path')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const User = require('./model/user')
+const bcrypt = require('bcryptjs')
+
+mongoose.connect('mongodb://localhost:27017/login-app-db',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 
 const app = express()
 app.use('/',express.static(path.join(__dirname,'static')))
 
-app.listen(9999, () => {
-    console.groupCollapsed('Server up at 9999')
+app.use(bodyParser.json())
+
+app.post('/api/register', async (req,res) => {
+    const{ username,password: plainTextPassword } = req.body
+
+    
+    const password = await bcrypt.hash(plainTextPassword,10)
+
+    try {
+        const responce = await User.create({
+            username,
+            password
+        })
+    } catch (error) {
+        console.log(error.message)
+        return res.json({status:'errror'})
+    } 
+    //Hashing the passwordds
+    res.json({status:'ok'})
 })
+ 
+app.listen(9999, () => {
+    console.log('Server up at 9999')
+})    
